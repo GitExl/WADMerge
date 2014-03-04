93236effb582a978fcc2002aa7dfe934462416d9
GitExl <dennis.meuwissen@gmail.com>
2014-03-04 22:12:40 +0100

    Document and clean up wad.

--- src/wad.d
+++ src/wad.d
@@ -33,6 +33,9 @@ import orderedaa;
 import util;
 
 
+// Valid WAD file types.
+// IWAD files (internal WADs) are only used as main game WADs. PWAD files (patch WADs) normally
+// contain lumps to extend or overwrite an IWAD's lumps with.
 public enum WADType : ubyte {
     IWAD,
     PWAD
@@ -40,27 +43,71 @@ public enum WADType : ubyte {
 
 
 public class Lump {
+
+    // The offset of this lump in the containing WAD file.
     private uint mOffset;
+
+    // The size of this lump's data, in bytes.
     private uint mSize;
+
+    // The name of this lump.
     private string mName;
+
+    // If true, this lump is marked as used in a lump list.
     private bool mIsUsed;
+
+    // The raw data of this lump.
     private ubyte[] mData;
 
 
+    /**
+     * Constructor for an empty lump.
+     */
     public this(string name) {
         this.mName = name;
     }
 
+    /**
+     * Constructor for a lump with predetermined data.
+     */
     public this(string name, ubyte[] data) {
         this.mName = name;
         this.mSize = data.length;
         this.mData = data;
     }
 
-    public this(uint offset, uint size, string name) {
+    /**
+     * Constructor for a lump whose data will be read at a later point.
+     */
+    public this(string name, uint size, uint offset) {
         this.mOffset = offset;
         this.mSize = size;
         this.mName = name;
+    }
+
+    /**
+     * Reads this lump's data from a Stream object.
+     *
+     * @param stream
+     * The Stream object to read the data from. The data will be read from the offset that is set
+     * in this lump. The amount of data to read is determined by the size of this lump.
+     */
+    public void readData(Stream stream) {
+        this.mData = new ubyte[this.mSize];
+
+        stream.seek(this.mOffset, SeekPos.Set);
+        stream.read(this.mData);
+    }
+
+    /**
+     * Puts new data in this lump.
+     *
+     * @param data
+     * The raw data to put in this lump. The lump's size will be updated to match the data's length.
+     */
+    public void putData(ubyte[] data) {
+        this.mData = data;
+        this.mSize = data.length;
     }
 
     public ubyte[] getData() {
@@ -94,41 +141,45 @@ public class Lump {
     public MemoryStream getStream() {
         return new MemoryStream(this.mData);
     }
-
-    public void readData(Stream stream) {
-        this.mData = new ubyte[this.mSize];
-
-        stream.seek(this.mOffset, SeekPos.Set);
-        stream.read(this.mData);
-    }
-
-    public void putData(ubyte[] data) {
-        this.mData = data;
-        this.mSize = data.length;
-    }
 }
 
+
 public class WAD {
+
+    // The type of this WAD file.
     private WADType mType;
+
+    // The number of lumps in this WAD file.
     private uint mLumpCount;
+
+    // The offset to the lump directory.
     private uint mDirectoryOffset;
 
+    // A list of the lumps inside this WAD file.
     private OrderedAA!(string,Lump) mLumps;
 
 
+    /**
+     * Constructor for creating a new WAD file.
+     */
     this(WADType type) {
         this.mType = type;
         this.mLumps = new OrderedAA!(string,Lump);
     }
 
+    /**
+     * Constructor for reading a WAD file from storage.
+     *
+     * @param fileName
+     * The name of the file to read this WAD from.
+     */
     this(string fileName) {
         char[4] id;
 
         BufferedFile file = new BufferedFile(fileName, FileMode.In);
-        file.read(cast(ubyte[])id);
-        file.read(this.mLumpCount);
-        file.read(this.mDirectoryOffset);
 
+        // Validate magic bytes, which should correspond to a known WAD type.
+        file.read(cast(ubyte[])id);
         if (id == "IWAD") {
             this.mType = WADType.IWAD;
         } else if (id == "PWAD") {
@@ -137,8 +188,11 @@ public class WAD {
             throw new Exception(format("%s is not a valid WAD file.", fileName));
         }
 
+        file.read(this.mLumpCount);
+        file.read(this.mDirectoryOffset);
+
         if (this.mDirectoryOffset > file.size()) {
-            throw new Exception(format("%s has a corrupted header.", fileName));
+            throw new Exception(format("%s has a corrupted or invalid header.", fileName));
         }
 
         readLumps(file);
@@ -146,32 +200,16 @@ public class WAD {
         file.close();
     }
 
-    private void readLumps(BufferedFile file) {
-        uint offset;
-        uint size;
-        string name;
-
-        ubyte[] directoryData = new ubyte[this.mLumpCount * 16];
-        file.seek(this.mDirectoryOffset, SeekPos.Set);
-        file.read(directoryData);
-
-        MemoryStream directory = new MemoryStream(directoryData);
-        this.mLumps = new OrderedAA!(string,Lump);
-
-        for (int index; index < this.mLumpCount; index++) {
-            directory.read(offset);
-            directory.read(size);
-            name = readPaddedString(directory, 8);
-
-            Lump lump = new Lump(offset, size, name);
-            lump.readData(file);
-
-            this.mLumps.add(lump.getName(), lump);
-        }
-    }
-
+    /**
+     * Writes this WAD's contents to a file.
+     *
+     * @param fileName
+     * The name of the file to write to. This file will be overwritten if it already exists.
+     */
     public void writeTo(string fileName) {
-        // Calculcate lump offsets.
+
+        // Calculcate lump offsets inside the WAD file.
+        // The directory follows the lump data.
         uint offset = 12;
         foreach (Lump lump; this.mLumps) {
             lump.setOffset(offset);
@@ -181,7 +219,7 @@ public class WAD {
 
         BufferedFile file = new BufferedFile(fileName, FileMode.OutNew);
 
-        // Write header.
+        // Write the file header.
         if (this.mType == WADType.IWAD) {
             file.write(cast(ubyte[])"IWAD");
         } else {
@@ -190,12 +228,12 @@ public class WAD {
         file.write(this.mLumpCount);
         file.write(this.mDirectoryOffset);
 
-        // Write lump data.
+        // Write raw lump data.
         foreach (Lump lump; this.mLumps) {
             file.write(lump.getData());
         }
 
-        // Write lump directory.
+        // Write the lump directory.
         foreach (Lump lump; this.mLumps) {
             file.write(lump.getOffset());
             file.write(lump.getSize());
@@ -205,8 +243,83 @@ public class WAD {
         file.close();
     }
 
+    /**
+     * Returns true if a lump name exists in this WAD.
+     *
+     * @param name
+     * The lump name to search for.
+     *
+     * @returns
+     * True if the lump is present, false otherwise.
+     */
+    public bool containsLump(string name) {
+        return this.mLumps.contains(name);
+    }
+
+    /**
+     * Adds a copy of another lump to this WAD file.
+     *
+     * @param other
+     * The lump to copy into this WAD file.
+     *
+     * @returns
+     * The new Lump object.
+     */
+    public Lump addLump(Lump other) {
+        Lump newLump = other;
+
+        this.mLumps.add(newLump.getName(), newLump);
+        this.mLumpCount += 1;
+
+        return newLump;
+    }
+
+    /**
+     * Adds an empty lump to this WAD file.
+     *
+     * @param name
+     * The name of the lump to add.
+     *
+     * @returns
+     * The new Lump object.
+     */
+    public Lump addLump(string name) {
+        Lump newLump = new Lump(name);
+
+        this.mLumps.add(newLump.getName(), newLump);
+        this.mLumpCount += 1;
+
+        return newLump;
+    }
+
+    private void readLumps(BufferedFile file) {
+        uint offset;
+        uint size;
+        string name;
+
+        // Read the raw directory data in one operation.
+        ubyte[] directoryData = new ubyte[this.mLumpCount * 16];
+        file.seek(this.mDirectoryOffset, SeekPos.Set);
+        file.read(directoryData);
+
+        MemoryStream directory = new MemoryStream(directoryData);
+        this.mLumps = new OrderedAA!(string,Lump);
+
+        // Create new lumps from the directory data and read their raw data.
+        for (int index; index < this.mLumpCount; index++) {
+            directory.read(offset);
+            directory.read(size);
+            name = readPaddedString(directory, 8);
+
+            Lump lump = new Lump(name, size, offset);
+            lump.readData(file);
+
+            this.mLumps.add(name, lump);
+        }
+    }
+
     public Lump getLump(const int index) {
-        if (index >= this.mLumps.length) {
+        if (index >= this.mLumps.length || index < 0) {
             return null;
         }
 
@@ -217,29 +330,7 @@ public class WAD {
         return this.mLumps.get(name, null);
     }
 
-    public bool containsLump(string name) {
-        return this.mLumps.contains(name);
-    }
-
     public OrderedAA!(string,Lump) getLumps() {
         return this.mLumps;
-    }
-    
-    public Lump addLump(Lump other) {
-        Lump newLump = other;
-
-        this.mLumps.add(newLump.getName(), newLump);
-        this.mLumpCount += 1;
-
-        return newLump;
-    }
-
-    public Lump addLump(string name) {
-        Lump newLump = new Lump(name);
-
-        this.mLumps.add(newLump.getName(), newLump);
-        this.mLumpCount += 1;
-
-        return newLump;
     }
 }
