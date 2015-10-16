0fbae7ea9b5bf6ff3ebc3aedd84c5b7a3e584f3d
GitExl <dennis.meuwissen@gmail.com>
2015-10-16 13:26:20 +0200

    Update copyright year.

--- src/textlumps.d
+++ src/textlumps.d
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014, Dennis Meuwissen
+    Copyright (c) 2015, Dennis Meuwissen
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
